4900d166b52220abfdbd46d8d6e0275a76bbe1b5
Dominik Dafert <dafnikdev@gmail.com>
2016-01-12 18:43:40 +0100

    Updated to TeleportAPI

--- RageMode/src/at/dafnik/ragemode/Commands/Teleport.java
+++ RageMode/src/at/dafnik/ragemode/Commands/Teleport.java
@@ -1,6 +1,5 @@
 package at.dafnik.ragemode.Commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -33,39 +32,30 @@ public boolean onCommand(CommandSender sender, Command cmd, String label, String
 			 			if(plugin.maps.contains(args[0].toLowerCase())){
 			 				String mapname = args[0].toLowerCase();
 			 				String chosenumber = args[1];
-			 				int spawnnumber = plugin.getConfig().getInt("ragemode.mapspawn." +  mapname + ".spawnnumber");
+			 				int wantto;
 			 				
-			 				if(Integer.valueOf(chosenumber) > 0 && Integer.valueOf(chosenumber) <= spawnnumber) {
-				 				String w = plugin.getConfig().getString("ragemode.mapspawn." + mapname + "." + chosenumber + ".world");
-								double x = plugin.getConfig().getDouble("ragemode.mapspawn." + mapname + "." + chosenumber + ".x");
-				 				double y = plugin.getConfig().getDouble("ragemode.mapspawn." + mapname + "." + chosenumber + ".y");
-				 				double z = plugin.getConfig().getDouble("ragemode.mapspawn." + mapname + "." + chosenumber + ".z");
-				 				double yaw = plugin.getConfig().getDouble("ragemode.mapspawn." + mapname + "." + chosenumber + ".yaw");
-				 				double pitch = plugin.getConfig().getDouble("ragemode.mapspawn." + mapname + "." + chosenumber + ".pitch");
-							
-				 				Location loc = new Location(Bukkit.getWorld(w), x, y, z);
-				 				loc.setYaw((float)yaw);
-				 				loc.setPitch((float)pitch);
-							
-				 				player.teleport(loc);
-			 				} else {
+			 				try {
+			 					wantto = Integer.valueOf(chosenumber);
+			 				} catch(ClassCastException e) {
 			 					player.sendMessage(Strings.error_enter_unknown_spawnnumber);
+			 					return true;
 			 				}
-			 			} else {
-			 				player.sendMessage(Strings.error_unknown_map);
-			 			}
-			 		} else {
-			 			player.sendMessage(Strings.error_enter);
-			 		}
+			 				
+			 				Location loc = new TeleportAPI(plugin).getMapSpawnLocation(wantto, mapname);
+			 				if(loc != null) player.teleport(loc);
+			 				else player.sendMessage(Strings.error_enter_unknown_spawnnumber);
+			 				
+			 			} else player.sendMessage(Strings.error_unknown_map);
+			 			
+			 		} else player.sendMessage(Strings.error_enter);
 			 	}
-			 	
+			 	return true;
 			}
 			 	 
 			if (cmd.getName().equalsIgnoreCase("tplobby")){
 				if(!(plugin.getPower(player) >= 5)){
 					player.sendMessage(Strings.error_permission);
 			 	}else{ 
-			 	
 					player.teleport(new TeleportAPI(plugin).getLobbyLocation());
 			 	}
 			}
