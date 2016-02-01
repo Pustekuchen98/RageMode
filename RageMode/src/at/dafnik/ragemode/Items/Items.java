44db1676b08815ff76e734c8abcdbb3f6d169a22
Dominik Dafert <dafnikdev@gmail.com>
2016-02-02 00:05:49 +0100

    Removed unused code

--- RageMode/src/at/dafnik/ragemode/Items/Items.java
+++ RageMode/src/at/dafnik/ragemode/Items/Items.java
@@ -51,10 +51,8 @@ public class Items {
 		ItemStack i3 = new ItemStack(Material.IRON_SPADE);
 		ItemMeta imd3 = i3.getItemMeta();
 		imd3.setDisplayName("§cKnife §8[§6Ready§8]");
-		//imd3.addEnchant(Enchantment.DAMAGE_ALL, 15, true);
 		i3.setItemMeta(imd3);
 		player.getInventory().setItem(1, i3);
-		
 
 		ItemStack i4 = new ItemStack(Material.DIAMOND_AXE);
 		ItemMeta imd4 = i4.getItemMeta();
