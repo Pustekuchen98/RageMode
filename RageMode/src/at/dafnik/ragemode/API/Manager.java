74f41fb886da5e5a03ee4744028798ced1f7d9e1
Dominik Dafert <dafnikdev@gmail.com>
2016-02-02 16:18:42 +0100

    Changed to static manager

--- RageMode/src/at/dafnik/ragemode/API/Manager.java
+++ RageMode/src/at/dafnik/ragemode/API/Manager.java
@@ -1,43 +1,34 @@
 package at.dafnik.ragemode.API;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
-import at.dafnik.ragemode.Main.Main;
-
 public class Manager {
 	
-	private Main plugin;
-	
-	public Manager(Main main) {
-		this.plugin = main;
-	}
-	
 	//DisplayName Manager
-	public void DisplayNameManagerMethode(Player player) {
+	public static void DisplayNameManagerMethode(Player player, String state) {
 		if (player.hasPermission("ragemode.admin")) player.setDisplayName("§4Admin §8| §4" + player.getName());
 		else if (player.hasPermission("ragemode.moderator")) player.setDisplayName("§cModerator §8| §c" + player.getName());
 		else if (player.hasPermission("ragemode.youtuber")) player.setDisplayName("§5YouTuber §8| §5" + player.getName());
 		else if (player.hasPermission("ragemode.premium")) player.setDisplayName("§6" + player.getName());		
 		else player.setDisplayName("§a" + player.getName());
 			
-		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-			@Override
-			public void run() {
-				if(!plugin.spectatorlist.contains(player)) player.setPlayerListName(player.getDisplayName());
-				else if(plugin.spectatorlist.contains(player)) player.setPlayerListName("§8[§4X§8]" + player.getDisplayName());	
-				else System.out.println(Strings.error_not_authenticated_player);	
-			}
-		}, 5);	
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+			
+		if (state == "normal") player.setPlayerListName(player.getDisplayName());
+		else if (state == "spectator")player.setPlayerListName("§8[§4X§8]" + player.getDisplayName());
+		else System.out.println(Strings.error_not_authenticated_player);	
 	}
 
 	// Helmet Manager
 	public static void HelmetManagerMethode(Player player) {
-		
 		ItemStack item = new ItemStack(Material.LEATHER_HELMET, 1);;
 		LeatherArmorMeta lam;
 		lam = (LeatherArmorMeta) item.getItemMeta();
