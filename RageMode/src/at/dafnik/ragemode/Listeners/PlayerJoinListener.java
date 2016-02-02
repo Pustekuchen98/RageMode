4a9d97a1d360840d4343283169c4e9a15da494a9
Dominik Dafert <dafnikdev@gmail.com>
2016-02-02 16:19:41 +0100

    Added hologram scheduler to PlayerJoinListener, optimized code

--- RageMode/src/at/dafnik/ragemode/Listeners/PlayerJoinListener.java
+++ RageMode/src/at/dafnik/ragemode/Listeners/PlayerJoinListener.java
@@ -24,9 +24,9 @@ import at.dafnik.ragemode.API.Manager;
 import at.dafnik.ragemode.API.Strings;
 import at.dafnik.ragemode.API.TeleportAPI;
 import at.dafnik.ragemode.API.Title;
-import at.dafnik.ragemode.Holograms.HologramsScheduler;
 import at.dafnik.ragemode.Main.Main;
 import at.dafnik.ragemode.Main.Main.Status;
+import at.dafnik.ragemode.MySQL.SQLCoins;
 import at.dafnik.ragemode.MySQL.SQLStats;
 
 public class PlayerJoinListener implements Listener{
@@ -42,35 +42,33 @@ public class PlayerJoinListener implements Listener{
 	}
 	
 	@EventHandler
-	public void Join(PlayerJoinEvent event){	
-		if(Main.status == Status.LOBBY || Main.status == Status.PRE_LOBBY) {
-			Player player = event.getPlayer();
-			
-			Manager manager = new Manager(plugin);
-			manager.DisplayNameManagerMethode(player);
-			
-			//Set standart Things
+	public void Join(PlayerJoinEvent event){
+		Player player = event.getPlayer();
+		
+		player.setHealth(20);
+		player.setSaturation(20);
+		player.setMaxHealth(20);
+		player.setWalkSpeed((float) 0.2);
+		player.setExp(0);
+		player.setLevel(0);
+		player.removePotionEffect(PotionEffectType.REGENERATION);
+		player.removePotionEffect(PotionEffectType.SPEED);
+		player.removePotionEffect(PotionEffectType.JUMP);
+		player.removePotionEffect(PotionEffectType.BLINDNESS);
+		player.removePotionEffect(PotionEffectType.SLOW);
+		player.removePotionEffect(PotionEffectType.INVISIBILITY);
+		player.setFireTicks(0);
+		player.getInventory().clear();
+		
+		if(Main.status == Status.LOBBY || Main.status == Status.PRE_LOBBY) {		
 			event.setJoinMessage(ChatColor.GRAY + "> §r" + player.getDisplayName() + ChatColor.DARK_AQUA + " joined the game");
-			player.setHealth(20);
-			player.setSaturation(20);
-			player.setMaxHealth(20);
-			player.setWalkSpeed((float) 0.2);
-			player.setExp(0);
-			player.setLevel(0);
-			player.removePotionEffect(PotionEffectType.REGENERATION);
-			player.removePotionEffect(PotionEffectType.SPEED);
-			player.removePotionEffect(PotionEffectType.JUMP);
-			player.removePotionEffect(PotionEffectType.BLINDNESS);
-			player.removePotionEffect(PotionEffectType.SLOW);
-			player.removePotionEffect(PotionEffectType.INVISIBILITY);
-			player.setFireTicks(0);
-			player.getInventory().clear();
+			
 			player.setGameMode(GameMode.SURVIVAL);
 			
-			//Chat Message
+			Manager.DisplayNameManagerMethode(player, "normal");
+			
 			if(plugin.getPower(player)> 0 ) player.sendMessage(Strings.lobby_rotate_your_mouse);
 			
-			//Tp to Lobby
 			Location loc = new TeleportAPI(plugin).getLobbyLocation();
 			if(loc == null) System.out.println(Strings.error_not_existing_lobbyspawn);
 			else player.teleport(loc);
@@ -91,41 +89,80 @@ public class PlayerJoinListener implements Listener{
 				     }
 				 }, 20);
 			}
-		    
-		    //Tablist Header and Footer
-		    Title.sendTabList(player , "§bRageMode");
+			
+			if(Main.isMySQL) {
+				SQLStats.createPlayer(player.getUniqueId().toString());
+				
+				Bukkit.getScheduler().runTaskLater(this.plugin, new Runnable(){
+					 public void run(){
+						 String w = plugin.getConfig().getString("ragemode.hologram.world");
+							double x = plugin.getConfig().getDouble("ragemode.hologram.x");
+							double y = plugin.getConfig().getDouble("ragemode.hologram.y");
+							double z = plugin.getConfig().getDouble("ragemode.hologram.z");
+							
+							Location loc = new Location(Bukkit.getWorld(w), x, y, z);
+							List<String> lines = new ArrayList<String>();
+							lines.add("§bRageMode §6Stats");
+							
+							int kills = SQLStats.getKills(player.getUniqueId().toString());
+							int deaths = SQLStats.getDeaths(player.getUniqueId().toString());
+							int playedgames = SQLStats.getPlayedGames(player.getUniqueId().toString());
+							int wongames = SQLStats.getWonGames(player.getUniqueId().toString());
+							int points = SQLStats.getPoints(player.getUniqueId().toString());
+							int coins = SQLCoins.getCoins(player.getUniqueId().toString());
+							int bowkills = SQLStats.getBowKills(player.getUniqueId().toString());
+							int knifekills = SQLStats.getKnifeKills(player.getUniqueId().toString());
+							int axtkills = SQLStats.getAxtKills(player.getUniqueId().toString());
+							int resets = SQLStats.getResets(player.getUniqueId().toString());
+							int suicides = SQLStats.getSuicides(player.getUniqueId().toString());
+							
+							float KD;
+							try {
+								KD = ((float) kills) / ((float) deaths);
+							} catch (ArithmeticException ex) {
+								KD = kills;
+							}
+							float rund = (float)(((int)(KD*100))/100.0);
+							
+							lines.add("§3Points§8: §e" + points);
+							lines.add("§3Coins§8: §e" + coins);
+							lines.add("§3Kills§8: §e" + kills);
+							lines.add("§3Bow Kills§8: §e" + bowkills);
+							lines.add("§3Knife Kills§8: §e" + knifekills);
+							lines.add("§3Axt Kills§8: §e" + axtkills);
+							lines.add("§3Suicides§8: §e" + suicides);
+							lines.add("§3Deaths§8: §e" + deaths);
+							lines.add("§3Kills/Deaths§8: §e" + rund);
+							lines.add("§3Played games§8: §e" + playedgames);
+							lines.add("§3Won games§8: §e" + wongames);
+							lines.add("§3Stats resets§8: §e" + resets);
+							
+							Holograms holo = new Holograms(loc, lines);
+							holo.display(player);	
+				     }
+				 }, 20*2);
+			}    
 		   
 		    Manager.HelmetManagerMethode(player);
+		 
+			Title.sendTabList(player, "§bRageMode");
+			Title.sendTitle(player, fadein, stay, fadeout, "§6Welcome in");
+			Title.sendSubtitle(player, fadein, stay, fadeout, "§bRageMode");
 		    
-		    //Title Send
-		    Title.sendTitle(player, fadein, stay, fadeout, "§6Welcome in");
-		    Title.sendSubtitle(player, fadein, stay, fadeout, "§bRageMode");
-		    
-		    if(Main.isMySQL) {
-		    	SQLStats.createPlayer(player.getUniqueId().toString());
-		    	//Create Hologramm		    	
-		    	new HologramsScheduler(plugin, player);
-		    }
 		    	   
 		} else if(Main.status == Status.INGAME){
-			
-			Player player = event.getPlayer();
-			
-			//Tp to Map
-			player.teleport(new TeleportAPI(plugin).getRandomMapSpawnLocations());
-			
-			event.setJoinMessage(null);
-			player.setGameMode(GameMode.SPECTATOR);
-			player.getInventory().clear();
 			plugin.spectatorlist.add(player);
 			
+			event.setJoinMessage(null);
+			
+			player.teleport(new TeleportAPI(plugin).getRandomMapSpawnLocations());
+			player.setGameMode(GameMode.SPECTATOR);		
+			
+			Manager.DisplayNameManagerMethode(player, "spectator");
+			
 			Title.sendTabList(player , "§bRageMode");
-			//Title Send
 		    Title.sendTitle(player, fadein, stay, fadeout, "§6Spectator");
 		    Title.sendSubtitle(player, fadein, 40, fadeout, "§bmode");
-		    
-		    Manager manager = new Manager(plugin);
-			manager.DisplayNameManagerMethode(player);
 			
 			Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable(){
 				 public void run(){
@@ -135,9 +172,7 @@ public class PlayerJoinListener implements Listener{
 			     }
 			 }, 20);
 			
-		} else {
-			System.out.println(Strings.error_unkown_gamestate);
-		}
+		} else System.out.println(Strings.error_unkown_gamestate);
 	}
 	
 	//Fly Speed Control
@@ -161,8 +196,7 @@ public class PlayerJoinListener implements Listener{
 	
 	//Kick Full Event
 	@EventHandler
-	public void PlayerPreLogin(PlayerLoginEvent event){
-			
+	public void PlayerPreLogin(PlayerLoginEvent event){		
 		if(Main.status == Status.WARMUP || Main.status == Status.WIN || Main.status == Status.RESTART){
 			event.disallow(Result.KICK_OTHER, Strings.error_cant_join_at_the_moment);
 			
