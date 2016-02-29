9a6b9c12d14c0221141de8f9fa4e4c084fb51474
Dominik Dafert <dafnikdev@gmail.com>
2016-02-29 12:47:40 +0100

    Optimzings

--- RageMode/src/at/dafnik/ragemode/Listeners/AsyncPlayerChatListener.java
+++ RageMode/src/at/dafnik/ragemode/Listeners/AsyncPlayerChatListener.java
@@ -21,20 +21,16 @@ public class AsyncPlayerChatListener implements Listener{
     public void onChat( AsyncPlayerChatEvent event ) {
 		Player player = event.getPlayer();
 		
-		if(!plugin.spectatorlist.contains(player)) {
-			event.setFormat(player.getDisplayName() + "§8: §f" + event.getMessage());
+		if(!plugin.spectatorlist.contains(player)) event.setFormat(player.getDisplayName() + "§8: §f" + event.getMessage());
 	
-		} else if(plugin.spectatorlist.contains(player)){
+		else if(plugin.spectatorlist.contains(player)) {
 			if(Main.status == Status.INGAME) {
 				event.setCancelled(true);
 				for(Player spectator : plugin.spectatorlist) {
 					spectator.sendMessage("§8[§4X§8]" + player.getDisplayName() + "§8: §f" + event.getMessage());
 				}
-			} else {
-				event.setFormat("§8[§4X§8]" + player.getDisplayName() + "§8: §f" + event.getMessage());
-			}
-		} else {
-			System.out.println(Strings.error_not_authenticated_player);
-		}
+			} else event.setFormat("§8[§4X§8]" + player.getDisplayName() + "§8: §f" + event.getMessage());
+
+		} else System.out.println(Strings.error_not_authenticated_player);
     }
 }
