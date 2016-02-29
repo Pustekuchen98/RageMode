f2bfb4096fcfaaccd5440a8bbd33e2e719fd6e33
Dominik Dafert <dafnikdev@gmail.com>
2016-02-29 12:45:57 +0100

    New string

--- RageMode/src/at/dafnik/ragemode/API/Strings.java
+++ RageMode/src/at/dafnik/ragemode/API/Strings.java
@@ -8,6 +8,7 @@ public class Strings {
 	private static String log_pre = "[RageMode] ";
 	private static String debug_pre = "[Debug]> ";
 	
+	public static String error_no_map = log_pre + "ERROR: TeleportAPI: getRandomMapSpawnLocation! Try catch faild!";
 	public static String error_explosion_no_killground = log_pre + "ERROR: ExplosionAPI doesn't work well. No Explosionground can found!";
 	public static String error_powerup_last = log_pre + "ERROR: PowerUP spawning failed! Caused by: No Map or no PowerUP Spawns";
 	public static String error_powerup_spawn = log_pre + "ERROR: PowerUP spawing failed! Caused by: Coordinates Error. Trying Spawn function again.";
