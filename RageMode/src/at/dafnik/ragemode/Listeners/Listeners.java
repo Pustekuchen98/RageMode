41fde5c5f514baf0bb22fb1023c7df00e5d10dd6
Dominik Dafert <dafnikdev@gmail.com>
2016-03-07 23:55:27 +0100

    Don't get damage if you fly with elytra into a wall

--- RageMode/src/at/dafnik/ragemode/Listeners/Listeners.java
+++ RageMode/src/at/dafnik/ragemode/Listeners/Listeners.java
@@ -125,13 +125,14 @@ public class Listeners implements Listener {
 			else if(Main.status == Main.Status.INGAME) event.setCancelled(false);
 			
 			if (event.getCause() == DamageCause.FALL) event.setDamage(0.0);
+			if(event.getCause() == DamageCause.FLY_INTO_WALL) event.setCancelled(true);
 	
 			if(entity instanceof Player) {
 				Player player = (Player) entity;
 				if(plugin.respawnsafe.contains(player)) {
 					event.setCancelled(true);
 				}
-			}
+			}			
 		}
 	}
 	
